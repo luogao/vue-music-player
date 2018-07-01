e26c0761cc5df5473f2cc56002975dc3142165a9
罗高 <luogao_lg@sina.com>
2018-07-01 22:50:49 +0800

    I feel so bad

--- src/components/Lyric.vue
+++ src/components/Lyric.vue
@@ -11,7 +11,7 @@ export default {
   name: 'lyric',
   data() {
     return {
-      lyric: null
+      lyric: null,
     }
   },
   computed: {
@@ -20,7 +20,20 @@ export default {
   async mounted () {
     const { id } = this.currentMusic
     const res = await Request.getLyric(id)
-    console.log(res)
-  }
+    console.log(res.data.lrc.lyric ? res.data.lrc.lyric : '暂无歌词')
+    this.parseLyric(res.data.lrc.lyric)
+  },
+  methods: {
+    parseLyric(lyric_s) {
+      if (!lyric_s) return []
+      lyric_s = lyric_s.replace(/([^\]^\n])\[/g, (match, p1) => p1 + '\n[')
+      const lyric = lyric_s.split('\n')
+      console.log(lyric)
+      lyric.forEach(item => {
+        const lrcTimes = item.match(/\[(\d{2}):(\d{2})(\.(\d{2,3}))?]/g)
+        console.log(lrcTimes)
+      })
+    },
+  },
 }
 </script>
